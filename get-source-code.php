bee3063c16334cef978308108d34988d99e3fea9
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-12-22 16:29:09 +0100

    update

--- get-source-code.php
+++ get-source-code.php
@@ -7,13 +7,13 @@ $zip_obj = new ZipArchive();
 $zip_obj->open('light-php.zip');
 $zip_obj->extractTo('system/temp_folder/');
 
-rename("system/temp_folder/light-php/system/engine", "system/engine");
-rename("system/temp_folder/light-php/index.php", "index.php");
-rename("system/temp_folder/light-php/config.rename.php", "config.rename.php");
-rename("system/temp_folder/light-php/php.rename.ini", "php.rename.ini");
-rename("system/temp_folder/light-php/.htaccess", ".htaccess");
-rename("system/temp_folder/light-php/.editorconfig", ".editorconfig");
-rename("system/temp_folder/light-php/post-install.php", "post-install.php");
-rename("system/temp_folder/light-php/.github", ".github");
+rename("system/temp_folder/Light-PHP-1.7/system/engine", "system/engine");
+rename("system/temp_folder/Light-PHP-1.7/index.php", "index.php");
+rename("system/temp_folder/Light-PHP-1.7/config.rename.php", "config.rename.php");
+rename("system/temp_folder/Light-PHP-1.7/php.rename.ini", "php.rename.ini");
+rename("system/temp_folder/Light-PHP-1.7/.htaccess", ".htaccess");
+rename("system/temp_folder/Light-PHP-1.7/.editorconfig", ".editorconfig");
+rename("system/temp_folder/Light-PHP-1.7/post-install.php", "post-install.php");
+rename("system/temp_folder/Light-PHP-1.7/.github", ".github");
 
 unlink("system/temp_folder/");
