fb26a9576fddc885daf0243a98fdeea6305be437
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-12-24 15:31:37 +0100

    minor change

--- src/controller/error/errorController.php
+++ src/controller/error/errorController.php
@@ -2,12 +2,15 @@
 
 namespace Controller;
 
-class errorController  
+use Library\Config;
+use Library\Output;
+
+class errorController
 {
     public function notFound()
     {
         $data = array();
-        $data['title'] = "Error! The Page: " . Config::get("url_action") . " couldnt be found!";
+        $data['title'] = "Error! The Page: " . Config::get("url_action") . " couldn't be found!";
         $data['body'] = "The page which you are looking for its not available, try searching in another place or try this later.";
 
         Output::load("error/notFoundView", $data);
