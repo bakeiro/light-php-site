249f54afa7cbf9daa1d058b7299fbfc1c27aa894
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2021-04-18 13:30:22 +0200

    update

--- get-source-code.php
+++ get-source-code.php
@@ -9,6 +9,10 @@ if (!file_exists("./system/engine") && !is_dir("./system/engine")){
     mkdir("./system/engine");
 }
 
+if (!file_exists("./system/config") && !is_dir("./system/config")){
+    mkdir("./system/config");
+}
+
 // Source code
 $source_code_version = "1.981";
 $source_code = file_get_contents("https://github.com/bakeiro/Light-PHP/archive/".$source_code_version.".zip");
@@ -22,7 +26,7 @@ $file_path = "./";
 $files = [".editorconfig", ".gitignore", ".htaccess", "package-lock.json", "package.json", "post-install.php"];
 foreach($files as $file) {
   $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file . ".php");
-  file_put_contents($file_path .$file . ".php", $temp_file);  
+  file_put_contents($file_path .$file . ".php", $temp_file);
 }
 
 // System
@@ -34,7 +38,7 @@ $file_path = "/system/config/";
 $files = ["config", "environment", "ini.php", "routes"];
 foreach($files as $file) {
   $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file . ".php");
-  file_put_contents("." . $file_path .$file . ".php", $temp_file);  
+  file_put_contents("." . $file_path .$file . ".php", $temp_file);
 }
 
 // Engine
@@ -42,7 +46,7 @@ $file_path = "/system/engine/";
 $files = ["Autoloader", "Container", "Controller", "Model", "Router", "SecureSession"];
 foreach($files as $file) {
   $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file . ".php");
-  file_put_contents("./" . $file_path .$file . ".php", $temp_file);  
+  file_put_contents("./" . $file_path .$file . ".php", $temp_file);
 }
 
 // Library
@@ -50,6 +54,6 @@ $file_path = "/system/library/";
 $files = ["Config", "Console", "Database", "Logger", "Output", "Session", "Util"];
 foreach($files as $file) {
   $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file . ".php");
-  file_put_contents("./" . $file_path .$file . ".php", $temp_file);  
+  file_put_contents("./" . $file_path .$file . ".php", $temp_file);
 }
 
