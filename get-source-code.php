b0c23ab10ac20ad9134470be3897390fc9a42dd6
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2021-04-18 13:38:52 +0200

    udpate

--- get-source-code.php
+++ get-source-code.php
@@ -22,11 +22,10 @@ $zip_obj = new ZipArchive();
 $zip_obj->open("light-php.zip");
 
 // Root
-$file_path = "./";
 $files = [".editorconfig", ".gitignore", ".htaccess", "package-lock.json", "package.json", "post-install.php"];
 foreach($files as $file) {
-  $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file);
-  file_put_contents($file_path .$file, $temp_file);
+  $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . "/" . $file);
+  file_put_contents("./" . $file, $temp_file);
 }
 
 // System
