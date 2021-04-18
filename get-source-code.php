4413eebc984e1a2b65045646f12034891a73f093
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2021-04-18 13:12:28 +0200

    update

--- get-source-code.php
+++ get-source-code.php
@@ -10,69 +10,50 @@ if (!file_exists("./system/engine") && !is_dir("./system/engine")){
 }
 
 // Source code
-$source_code_version = "1.85";
+$source_code_version = "1.981";
 $source_code = file_get_contents("https://github.com/bakeiro/Light-PHP/archive/".$source_code_version.".zip");
 file_put_contents("./light-php.zip", $source_code);
 
 $zip_obj = new ZipArchive();
 $zip_obj->open("light-php.zip");
 
-// Root files
-$index_php_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/index.php");
-file_put_contents("./index.php", $index_php_code);
+// Root
+$file_path = "/";
+$files = [".editorconfig", ".gitignore", ".htaccess", "package-lock.json", "package.json", "post-install.php"];
+foreach($files as $file) {
+  $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file . ".php");
+  file_put_contents("./" . $file_path .$file . ".php", $temp_file);  
+}
 
-$config_php_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/config.rename.php");
-file_put_contents("./config.rename.php", $config_php_code);
+// System
+$route_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/logs/.gitkeep");
+file_put_contents("./system/logs/.gitkeep", $route_code);
 
-$ini_php_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/php.rename.ini");
-file_put_contents("./php.rename.ini", $ini_php_code);
+// Logs
+$startup_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/bootstrap.php");
+file_put_contents("./system/bootstrap.php", $startup_code);
 
-$editorconfig_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/.editorconfig");
-file_put_contents("./.editorconfig", $editorconfig_code);
+// Config
+$file_path = "/system/config/";
+$files = ["config", "environment", "ini.php", "routes"];
+foreach($files as $file) {
+  $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file . ".php");
+  file_put_contents("./" . $file_path .$file . ".php", $temp_file);  
+}
 
-$htaccess_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/.htaccess");
-file_put_contents("./.htaccess", $htaccess_code);
+// Engine
+$file_path = "/system/engine/";
+$files = ["Autoloader", "Container", "Controller", "Model", "Router", "SecureSession"];
+foreach($files as $file) {
+  $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file . ".php");
+  file_put_contents("./" . $file_path .$file . ".php", $temp_file);  
+}
 
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/post-install.php");
-file_put_contents("./post-install.php", $post_install_code);
+// Library
+$file_path = "/system/library/";
+$files = ["Config", "Console", "Database", "Logger", "Output", "Session", "Util"];
+foreach($files as $file) {
+  $temp_file = $zip_obj->getFromName("Light-PHP-" . $source_code_version . $file_path . $file . ".php");
+  file_put_contents("./" . $file_path .$file . ".php", $temp_file);  
+}
 
-$route_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/routes.php");
-file_put_contents("./system/routes.php", $route_code);
-
-$startup_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/startup.php");
-file_put_contents("./system/startup.php", $startup_code);
-
-// Engine files
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/engine/Autoloader.php");
-file_put_contents("./system/engine/Autoloader.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/engine/Controller.php");
-file_put_contents("./system/engine/Controller.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/engine/Errors.php");
-file_put_contents("./system/engine/Errors.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/engine/Router.php");
-file_put_contents("./system/engine/Router.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/engine/SessionSecureHandler.php");
-file_put_contents("./system/engine/SessionSecureHandler.php", $post_install_code);
-
-// Library 
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/library/Console.php");
-file_put_contents("./system/library/Console.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/library/Database.php");
-file_put_contents("./system/library/Database.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/library/Output.php");
-file_put_contents("./system/library/Output.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/library/Session.php");
-file_put_contents("./system/library/Session.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/library/Util.php");
-file_put_contents("./system/library/Util.php", $post_install_code);
-
-$post_install_code = $zip_obj->getFromName("Light-PHP-".$source_code_version."/system/library/Config.php");
-file_put_contents("./system/library/Config.php", $post_install_code);
